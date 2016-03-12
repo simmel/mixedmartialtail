59be454d5c22284e4baa919e2333cc29062739de
Simon Lundström <simmel@soy.se>
2016-03-12 01:22:11 +0100

    Add a basic setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+'''
+mixedmartialtail: a warlike approach to tailing logs with mixed formats
+
+Copyright 2016-, Simon Lundström <simmel@soy.se>.
+Licensed under the ISC license.
+'''
+
+import sys
+from setuptools import setup
+
+version = "1.0.0"
+
+setup(
+    name="mixedmartialtail",
+    version=version,
+    description="a warlike approach to tailing logs with mixed formats",
+    long_description=open("README.md").read(),
+    author="Simon Lundström",
+    author_email="simmel@soy.se",
+    url="https://github.com/simmel/mixedmartialtail",
+    license="ISC license",
+    entry_points={
+        'console_scripts': [
+            'mmt=mixedmartialtail:main'
+        ]
+    }
+)
