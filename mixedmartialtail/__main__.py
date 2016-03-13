07d3772d04e6e79fcf3cd006beda6b0c2d3e243a
Simon Lundström <simmel@soy.se>
2016-03-13 19:14:04 +0100

    Use init and main files correct.
    Python is hard = S

--- mixedmartialtail/__main__.py
+++ mixedmartialtail/__main__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8 sw=4 et tw=79
-import sys
-import io
-
-def main():
-    input = io.open(sys.stdin.fileno(), 'r', encoding='utf-8', errors='replace')
-    for line in input:
-        sys.stdout.write(line)
+from . import main
 
 if __name__ == "__main__":
     main()
