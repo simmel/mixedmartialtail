02b3a409c5d661ef42063ddf8f9bfb7573741525
Simon Lundström <simmel@soy.se>
2017-03-30 21:58:49 +0200

    Add json tests
    Just log4j2 for now

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+# vim: set fileencoding=utf-8 sw=4 et tw=79
+# From https://mail.python.org/pipermail/pytest-dev/2013-June/002295.html
+# https://gist.github.com/pfctdayelise/5719730
+def pytest_generate_tests(metafunc):
+    """
+    If the test_ fn has a idparametrize mark, use it to create parametrized
+    tests with ids. Instead of giving a list of argvals (test values), it
+    should be a dict of test id strings -> tuple of test values
+    e.g.
+    @pytest.mark.idparametrize(('a', 'b'), {
+        'foo': (1, 2),
+        'bar': (3, 4),
+    })
+    """
+
+    idparametrize = getattr(metafunc.function, 'idparametrize', None)
+    if idparametrize:
+        argnames, testdata = idparametrize.args
+        ids, argvalues = zip(*testdata.items())
+        metafunc.parametrize(argnames, argvalues, ids=ids)
---
+++ tests/test_plugin_input_json.py
@@ -0,0 +1,24 @@
+# vim: set fileencoding=utf-8 sw=4 et tw=79
+import mixedmartialtail
+from mock import patch
+import io
+import json
+import pytest
+
+syslog = u'2005-04-12T17:03:45.000Z sarena.waza.se '
+log4j_json = u'''{"timeMillis":1487684052412,"thread":"main","level":"INFO","loggerName":"App","message":"🔣This is a log message🆒, there's no other like it.㊙️","endOfBatch":false,"loggerFqcn":"org.apache.logging.log4j.spi.AbstractLogger","threadId":1,"threadPriority":5}'''
+
+test_parameters = {
+        'log4j2': (
+            u'{syslog}{json_log}\n'.format(syslog=syslog, json_log=log4j_json),
+            u'{syslog}{message}\n'.format(syslog=syslog, message=json.loads(log4j_json)['message']),
+        ),
+    }
+
+@pytest.mark.idparametrize('input,expected', test_parameters)
+def test_json_uses_correct_message(capsys, input, expected):
+    with patch.object(mixedmartialtail, 'get_input', return_value=io.StringIO(input)):
+        mixedmartialtail.main(argv=[])
+    out, err = capsys.readouterr()
+    assert out == expected
+    assert err == ""
