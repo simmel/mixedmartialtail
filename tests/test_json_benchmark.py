b253c6b7c5170bb93607d3c5cbbb70c16752a602
Simon Lundström <simmel@soy.se>
2018-02-12 22:13:56 +0100

    Add micro benchmark for json plugin

--- tests/test_json_benchmark.py
+++ tests/test_json_benchmark.py
@@ -1,10 +1,18 @@
 # vim: set fileencoding=utf-8 sw=4 et tw=79
+import mixedmartialtail
 import pytest
-import time
+import io
+from mock import patch
+from test_variables import *
 
 @pytest.mark.benchmark(
     min_rounds=5,
 )
 
-def test_json_100p_syslog(benchmark):
-    benchmark(time.sleep, 0.02)
+@pytest.mark.idparametrize('input,expected_syslog,expected_json', test_parameters)
+def test_json_one_line(capsys, benchmark, input, expected_syslog, expected_json):
+    with patch.object(mixedmartialtail, 'get_input', return_value=io.StringIO(input)):
+        benchmark(mixedmartialtail.main, argv=[])
+    out, err = capsys.readouterr()
+    assert out == expected_syslog
+    assert err == ""
